fbc1f14360a6dadfef25d624fdc9d5e9926d8c4b
Emmett McDow <emcdow@vmware.com>
2023-05-17 16:11:00 -0700

    bootstrap that works the first time, trailing whitespace, updated vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,75 +1,125 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
-
-git pull origin main;
-
 function packageManager() {
-	which dnf && PM='dnf' && return 0
-	which yum && PM='yum' && return 0
-	which apt && PM='apt' && return 0
-	which brew && PM='brew' && return 0
-	return 1
+    which dnf &>/dev/null && PM='dnf' && return 0
+    which yum &>/dev/null && PM='yum' && return 0
+    which apt &>/dev/null && PM='apt' && return 0
+    which brew &>/dev/null && PM='brew' && return 0
+    return 1
 }
 
 function assertZSH() {
-	which zsh && return 0
-	
-	if packageManager; then
-		echo "You will be asked to input your sudo password"
-	else
-		echo "failed to install zsh, exiting"
-		return 1
-	fi
+    echo "---"
+    which zsh &>/dev/null && echo "✅ - zsh present" && return 0
 
-	sudo $PM install -y zsh || echo "failed to install zsh, exiting" && return 1
-	sudo chsh -s $(which zsh) $USER
-	return 0
+    echo "❌ - zsh not present, attempting to install"
+    if ! packageManager; then
+        echo "failed to install zsh, exiting"
+        return 1
+    fi
+
+    if sudo $PM install -y zsh  &>/dev/null ; then
+        echo "✅ - zsh installed"
+        return 0
+    else
+        echo "failed to install zsh, exiting"
+        return 1
+    fi
 }
 
 function assertOHMY() {
-	# Check for oh-my-zsh
-  [ -d "$HOME/.oh-my-zsh" ] && return 0
+    # Check for oh-my-zsh
+    echo "---"
+    [ -d "$HOME/.oh-my-zsh" ] && echo "✅ - oh-my-zsh present" && return 0
 
-	# Install oh-my-zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && return 0
+    echo "❌ - oh-my-zsh not present, attempting to install"
+    # Install oh-my-zsh
+    curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > install.sh
+    chmod +x install.sh
+    zsh -c "export SHELL=`which zsh`;export RUNZSH=no; . ./install.sh &>/dev/null" &>/dev/null && rm install.sh && echo "✅ - oh-my-zsh installed" && return 0
 
-	# Failed
-	echo "failed to install oh-my-zsh, exiting"
-	return 1
+    rm install.sh
+    # Failed
+    echo "failed to install oh-my-zsh, exiting"
+    return 1
 }
 
 function assertPyEnv() {
-	# Check for PyEnv
-	which pyenv && return 0
-  
-	# Install PyEnv
-	bash -c "$(curl https://pyenv.run)" && return 0
-	
-	# Failed
-	echo "failed to install pyenv, exiting"
-	return 1
+    echo "---"
+    # Check for PyEnv
+    [ -d $HOME/.pyenv ] && echo "✅ - PyEnv present" && return 0
 
+
+    echo "❌ - PyEnv not present, attempting to install"
+    # Install PyEnv
+    curl -fsSL https://pyenv.run > install.sh
+    chmod +x install.sh
+    bash -c ". ./install.sh &>/dev/null" &>/dev/null && rm install.sh && echo "✅ - PyEnv installed" && return 0
+
+    rm install.sh
+    # Failed
+    echo "failed to install pyenv, exiting"
+    return 1
+
+}
+
+function assertNode() {
+    # This is a dependency for some vim plugins
+    echo "---"
+    which node &>/dev/null && echo "✅ - Node present" && return 0
+
+    echo "❌ - Node not present, attempting to install"
+    case $PM in
+        dnf)
+            sudo dnf module install -y nodejs:20/common &>/dev/null || echo "failed to install node, exiting"
+
+        ;;
+        yum)
+            sudo yum module install -y nodejs:20/common &>/dev/null || echo "failed to install node, exiting"1
+
+        ;;
+        apt)
+            sudo apt install -y nodejs  &>/dev/null || echo "failed to install node, exiting"
+        ;;
+        brew)
+            brew install nodejs -y &>/dev/null || echo "failed to install node, exiting"
+        ;;
+        *)
+            echo "could not install node, exiting"
+        ;;
+    esac
+    which node &> /dev/null || return 1
+    echo "✅ - Node Installed"
+    return 0
 }
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.zshrc;
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude ".osx" \
+        --exclude "bootstrap.sh" \
+        --exclude "README.md" \
+        --exclude "LICENSE-MIT.txt" \
+        --exclude "Dockerfile" \
+        -avh --no-perms . ~;
+
+    [[ $SHELL == `which zsh` ]] && source ~/.zshrc
+
+    echo "Checks done and sync completed, make sure you run zsh and source .zshrc"
 }
 
+
+# "Main" function
+cd "$(dirname "${BASH_SOURCE}")";
+git pull origin main &>/dev/null;
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-		assertZSH && assertOHMY && assertPyEnv && doIt;
+        assertZSH && assertOHMY && assertPyEnv && assertNode && doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		assertZSH && assertOHMY && assertPyEnv && doIt;
-	fi;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        assertZSH && assertOHMY && assertPyEnv && assertNode && doIt;
+    fi;
 fi;
 unset doIt;
